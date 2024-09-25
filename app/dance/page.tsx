5b6f5de9d2d4ec0da914847d14d49c090c17058e
imkiva <imkiva@islovely.icu>
2024-09-25 19:18:50 +0800

    misc: polish video player

--- app/dance/page.tsx
+++ app/dance/page.tsx
@@ -1,37 +1,26 @@
-"use client"
+"use client";
 
-import { nextVideo, selectPlayList } from "@/store/modules/playList"
-import { useCallback, useMemo } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import PlyrWrapper from "@/components/PlyrWrapper"
+import { nextVideo, selectPlayList } from "@/store/modules/playList";
+import { useCallback, useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import PlyrWrapper from "@/components/PlyrWrapper";
 
 export default function DancePage() {
-  const { playList } = useSelector(selectPlayList)
-  const dispatch = useDispatch()
-  const song = useMemo(() => {
-    if (playList.length === 0) return null
-    return playList[0]
-  }, [playList])
-  const currentVideoUrl = useMemo(() => {
-    if (song === null) return ""
-    return `https://api.udon.dance/Api/Songs/play?id=${song.id}`
-  }, [song])
-  const flip = useMemo(() => {
-    if (song === null) return undefined
-    return song.flip
-  }, [song])
+  const { playList } = useSelector(selectPlayList);
+  const dispatch = useDispatch();
+  const video = useMemo(() => playList[0] ?? null, [playList]);
 
   const onVideoEnded = useCallback(() => {
-    console.log("onVideoEnded")
-    dispatch(nextVideo())
-  }, [dispatch])
+    console.log("onVideoEnded");
+    dispatch(nextVideo());
+  }, [dispatch]);
+
   return (
     <>
       <PlyrWrapper
-        videoUrl={currentVideoUrl}
-        flip={flip}
+        video={video}
         onVideoEnded={onVideoEnded}
       />
     </>
-  )
+  );
 }
--- components/PlyrWrapper/index.tsx
+++ components/PlyrWrapper/index.tsx
@@ -1,76 +1,87 @@
-import React, { useEffect, useRef } from "react"
-import Plyr from "plyr"
-import "plyr/dist/plyr.css"
+import React, { useEffect, useRef } from "react";
+import Plyr from "plyr";
+import "plyr/dist/plyr.css";
+import { GenericVideo } from "@/types/video";
+import { delay } from "lodash";
 
 interface VideoPlayerProps {
-  videoUrl: string
-  flip?: boolean
-  onVideoEnded?: () => void
+  video: GenericVideo | null;
+  onVideoEnded: () => void;
 }
 
 const VideoPlayer: React.FC<VideoPlayerProps> = ({
-  videoUrl,
-  flip = false,
-  onVideoEnded,
+  video,
+  onVideoEnded
 }) => {
-  console.log(videoUrl)
-  const videoRef = useRef<HTMLVideoElement | null>(null)
-  const plyrInstance = useRef<Plyr | null>(null)
+  const videoUrl = video ? `https://api.udon.dance/Api/Songs/play?id=${video.id}` : "";
+  const flip = video?.flip ?? false;
+
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const plyrInstance = useRef<Plyr | null>(null);
 
   useEffect(() => {
+    if (videoUrl === "") return;
+
+    console.log("Got new video url", videoUrl);
     if (videoRef.current) {
       plyrInstance.current = new Plyr(videoRef.current, {
-        autoplay: true,
-      })
+        // IMPORTANT: no autoplay/autopause here, we will handle it manually,
+        // or race condition happens when the first video is loaded:
+        // the video starts to play, and seconds later the video is paused.
+        autoplay: false,
+        autopause: false
+      });
 
-      plyrInstance.current.on("ended", () => {
-        if (onVideoEnded) {
-          console.log(111)
-          onVideoEnded()
-        }
-      })
+      plyrInstance.current.on("ended", () => onVideoEnded());
+      plyrInstance.current.on("canplay", () => {
+        // This enforces the video to play when the video is loaded;
+        // the "autoplay" option works only on the first video,
+        // for videos that are loaded after the first one, we need to call play() manually
+        delay(() => {
+          if (plyrInstance.current) {
+            plyrInstance.current.play();
+          }
+        }, 10000);
+      });
 
-      muteAndUnmuteAfterDelay()
-      applyFlipEffect()
+      muteAndUnmuteAfterDelay();
+      applyFlipEffect();
+      // This enforces the video to reload when the videoUrl changes
+      videoRef.current.load();
     }
-
-    return () => {
-      if (plyrInstance.current) {
-        plyrInstance.current.destroy()
-      }
-    }
-  }, [videoUrl])
+  }, [videoUrl]);
 
   const muteAndUnmuteAfterDelay = (delay = 1000) => {
     if (plyrInstance.current) {
-      plyrInstance.current.muted = true
+      plyrInstance.current.muted = true;
       setTimeout(() => {
         if (plyrInstance.current) {
-          plyrInstance.current.muted = false
-          plyrInstance.current.volume = 1
+          plyrInstance.current.muted = false;
+          plyrInstance.current.volume = 1;
         }
-      }, delay)
+      }, delay);
     }
-  }
+  };
 
   const applyFlipEffect = () => {
     if (videoRef.current) {
       const wrapper = videoRef.current.closest(
         ".plyr__video-wrapper"
-      ) as HTMLElement
+      ) as HTMLElement;
       if (wrapper) {
-        wrapper.style.transform = flip ? "scaleX(-1)" : "scaleX(1)"
+        wrapper.style.transform = flip ? "scaleX(-1)" : "scaleX(1)";
       }
     }
-  }
+  };
 
   return (
     <div className="w-full h-full">
+      <h1>{video && `${video.id}. ${video.composedTitle}`}</h1>
       <video ref={videoRef} controls className="plyr__video-embed">
         <source src={videoUrl} type="video/mp4" className=" w-full h-full" />
       </video>
     </div>
-  )
-}
+  );
+};
 
-export default VideoPlayer
+export default VideoPlayer;
