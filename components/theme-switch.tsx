eb1c89cf705cac73aa1acda943ddb2a9e2ac488e
Echo <chroom@foxmail.com>
2024-10-06 17:27:13 +0800

    chore: remove api ayainfo and udoninfo

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -1,29 +1,29 @@
-"use client";
+"use client"
 
-import { FC } from "react";
-import { VisuallyHidden } from "@react-aria/visually-hidden";
-import { SwitchProps, useSwitch } from "@nextui-org/switch";
-import { useTheme } from "next-themes";
-import { useIsSSR } from "@react-aria/ssr";
-import clsx from "clsx";
+import { FC } from "react"
+import { VisuallyHidden } from "@react-aria/visually-hidden"
+import { SwitchProps, useSwitch } from "@nextui-org/switch"
+import { useTheme } from "next-themes"
+import { useIsSSR } from "@react-aria/ssr"
+import clsx from "clsx"
 
-import { SunFilledIcon, MoonFilledIcon } from "@/components/icons";
+import { SunFilledIcon, MoonFilledIcon } from "@/assets/icon"
 
 export interface ThemeSwitchProps {
-  className?: string;
-  classNames?: SwitchProps["classNames"];
+  className?: string
+  classNames?: SwitchProps["classNames"]
 }
 
 export const ThemeSwitch: FC<ThemeSwitchProps> = ({
   className,
   classNames,
 }) => {
-  const { theme, setTheme } = useTheme();
-  const isSSR = useIsSSR();
+  const { theme, setTheme } = useTheme()
+  const isSSR = useIsSSR()
 
   const onChange = () => {
-    theme === "light" ? setTheme("dark") : setTheme("light");
-  };
+    theme === "light" ? setTheme("dark") : setTheme("light")
+  }
 
   const {
     Component,
@@ -36,7 +36,7 @@ export const ThemeSwitch: FC<ThemeSwitchProps> = ({
     isSelected: theme === "light" || isSSR,
     "aria-label": `Switch to ${theme === "light" || isSSR ? "dark" : "light"} mode`,
     onChange,
-  });
+  })
 
   return (
     <Component
@@ -44,7 +44,7 @@ export const ThemeSwitch: FC<ThemeSwitchProps> = ({
         className: clsx(
           "px-px transition-opacity hover:opacity-80 cursor-pointer",
           className,
-          classNames?.base,
+          classNames?.base
         ),
       })}
     >
@@ -66,7 +66,7 @@ export const ThemeSwitch: FC<ThemeSwitchProps> = ({
               "px-0",
               "mx-0",
             ],
-            classNames?.wrapper,
+            classNames?.wrapper
           ),
         })}
       >
@@ -77,5 +77,5 @@ export const ThemeSwitch: FC<ThemeSwitchProps> = ({
         )}
       </div>
     </Component>
-  );
-};
+  )
+}
